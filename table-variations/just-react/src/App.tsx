fe38a5c45ce01f5a0e7cb13aa69bb2f43fdb8fc6
ctarrington <arrington.ct@gmail.com>
2022-02-15 23:04:14 -0500

    100k w 50 fields at 1Hz

--- table-variations/just-react/src/App.tsx
+++ table-variations/just-react/src/App.tsx
@@ -3,15 +3,20 @@ import "./App.css";
 import { Alien } from "./Alien";
 import { useInterval } from "usehooks-ts";
 
-const FIELD_COUNT = 10;
-const ROW_INCREASE = 100;
+const PAGE_SIZE = 20;
+const FIELD_COUNT = 50;
+const ROW_MAX = 100*1000;
+const ROW_INCREASE = 5*1000;
 
 function App() {
-  const [aliens, setAliens] = useState<Alien[]>([new Alien(1, FIELD_COUNT)]);
+  const [aliens, setAliens] = useState<Alien[]>([new Alien(0, FIELD_COUNT)]);
   const [count, setCount] = useState<number>(0);
+  const [sortField, setSortField] = useState<number>(0);
+  const [pageIndex, setPageIndex] = useState<number>(0);
+  const [tick, setTick] = useState<number>(0);
 
   const headers = aliens[0].fields.map((field, index) => {
-    return <th> {field.name} </th>;
+    return <th> <a onClick={() => {setSortField(index)}}>{field.name}</a> </th>;
   });
   const alienHeader = (
     <tr>
@@ -20,7 +25,19 @@ function App() {
     </tr>
   );
 
-  const alienSummary = aliens.map((alien) => {
+  const sortedAliens = aliens.sort((alienA, alienB) => {
+    return alienA.fields[sortField].value - alienB.fields[sortField].value;
+  })
+
+  const restrictedAliens = [];
+  for (let index=pageIndex*PAGE_SIZE; index < (pageIndex+1)*PAGE_SIZE; index++) {
+    if (!sortedAliens[index]) {
+      break;
+    }
+    restrictedAliens.push(sortedAliens[index]);
+  }
+
+  const alienSummary = restrictedAliens.map((alien) => {
     const fields = alien.fields.map((field, index) => {
       return <td key={index}> {field.value.toFixed(4)} </td>;
     });
@@ -30,13 +47,19 @@ function App() {
     </tr>;
   });
 
+  const pageCount = Math.ceil(aliens.length / PAGE_SIZE);
+
   useInterval(() => {
+    setTick(tick + 1);
+
     for (let alien of aliens) {
       alien.tick();
     }
 
-    for (let index = 0; index<ROW_INCREASE; index++) {
-      aliens.push(new Alien(aliens.length, FIELD_COUNT));
+    if (aliens.length < ROW_MAX) {
+      for (let index = 0; index < ROW_INCREASE; index++) {
+        aliens.push(new Alien(aliens.length, FIELD_COUNT));
+      }
     }
 
     setAliens(aliens);
@@ -46,10 +69,16 @@ function App() {
   return (
     <div className="App">
       <h3>{count}</h3>
+      <div>
+        <button onClick={() => { setPageIndex(Math.max(0, pageIndex -1))}}>Page Down</button>
+        {pageIndex} of {pageCount - 1}
+        <button onClick={() => { setPageIndex(Math.min(pageIndex +1, pageCount))}}>Page Up</button>
+      </div>
       <table>
-      {alienHeader}
-      {alienSummary}
+        {alienHeader}
+        {alienSummary}
       </table>
+      <div>{tick}</div>
     </div>
   );
 }
