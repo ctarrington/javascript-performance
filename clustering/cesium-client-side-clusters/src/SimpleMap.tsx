425abf019cced5f47b1d6bf0b2710d5a825084c8
CT Arrington <arrington.ct@gmail.com>
2025-11-27 10:12:20 -0500

    cesium with supercluster, basics

--- clustering/cesium-client-side-clusters/src/SimpleMap.tsx
+++ clustering/cesium-client-side-clusters/src/SimpleMap.tsx
@@ -1,25 +1,36 @@
 import {createViewer} from "./createViewer.ts";
-import {useEffect, useRef} from "react";
-import {Color, GeoJsonDataSource, Viewer} from "cesium";
-import {asGeoJson, createSymetricData} from "./dataFactory.ts";
+import {useEffect, useRef, useState} from "react";
+import {GeoJsonDataSource, Viewer, Math as CesiumMath} from "cesium";
+
+const worker = new Worker(new URL("./getClustersWorker.ts", import.meta.url), {type: "module"});
 
 export default function SimpleMap() {
+    const [tick, setTick] = useState(0);
+    const lastRefresh = useRef(0);
+
     const mapRef = useRef(null);
     const viewerRef = useRef<Viewer>(null);
+    const geoJsonDataSourceRef = useRef<GeoJsonDataSource>(null);
 
     useEffect(() => {
         if (mapRef.current) {
             viewerRef.current = createViewer(mapRef.current);
-            const geoJson = asGeoJson(createSymetricData(400, 12, 20, 50, -80, 0));
-            GeoJsonDataSource.load(geoJson, {
-                strokeWidth: 5,
-                markerSymbol: 'circle',
-            }).then(function (dataSource) {
-                dataSource.clustering.enabled = true;
-                viewerRef.current?.dataSources.add(dataSource);
-            }).catch(function (error) {
-                console.error('Error loading GeoJSON:', error);
-            });
+            geoJsonDataSourceRef.current = new GeoJsonDataSource("clusters");
+            viewerRef.current.dataSources.add(geoJsonDataSourceRef.current);
+
+            worker.onmessage = (event: MessageEvent<any>) => {
+                if (event.data.message === "refreshed" || event.data.message === "moved") {
+                    console.log("QQQ sm received clusters", event.data.content);
+                    const features = event.data.content;
+                    const geoJson = {
+                        type: "FeatureCollection",
+                        features,
+                    };
+                    geoJsonDataSourceRef.current?.load(geoJson);
+                } else if (event.data.message === "expansion") {
+                    console.log("QQQ sm received expansion zoom", event.data.content.expansionZoom);
+                }
+            };
         }
 
         return () => {
@@ -30,6 +41,39 @@ export default function SimpleMap() {
             }
         }
     }, []);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTick(Date.now());
+        }, 1_000);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (mapRef.current === null || viewerRef.current === null) {
+            return;
+        }
+
+        if (Date.now() - lastRefresh.current < 10_000) {
+            return;
+        }
+
+        const bounds = viewerRef.current.camera.computeViewRectangle();
+        const bbox = bounds ? [
+            CesiumMath.toDegrees(bounds.west),
+            CesiumMath.toDegrees(bounds.south),
+            CesiumMath.toDegrees(bounds.east),
+            CesiumMath.toDegrees(bounds.north),
+        ] : [-179.9, -89.9, 179.9, 89.9];
+        worker.postMessage({
+            command: "refreshClusters",
+            bbox,
+            zoom: 3,
+        });
+    }, [tick]);
 
     return (
         <div className="SimpleMap" ref={mapRef}></div>
---
+++ clustering/cesium-client-side-clusters/src/getClustersWorker.ts
@@ -0,0 +1,61 @@
+import {createSymetricData, type LatLonArray} from "./dataFactory.ts";
+import Supercluster from "supercluster";
+import type {AnyProps, PointFeature} from "supercluster";
+
+const latLonArray: LatLonArray = createSymetricData(
+    1_000_000,
+    10,
+    -70,
+    70,
+    -120,
+    120,
+);
+
+let superCluster: Supercluster;
+const startTime = performance.now();
+
+self.onmessage = (event: MessageEvent<any>) => {
+    const deltaSeconds = (performance.now() - startTime) / 1000;
+    if (event.data.command === "refreshClusters") {
+        const features: PointFeature<AnyProps>[] = latLonArray.map((latLon) => {
+            const lon = latLon[1] + deltaSeconds * 0.1;
+            const adjustedLon = lon > 180 ? -180 : lon;
+            return {
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: [adjustedLon, latLon[0]],
+                },
+                properties: {},
+            };
+        });
+        superCluster = new Supercluster({
+            log: false,
+            radius: 60,
+            extent: 256,
+            maxZoom: 7,
+        }).load(features);
+
+        self.postMessage({
+            message: "refreshed",
+            content: superCluster.getClusters(event.data.bbox, event.data.zoom),
+        });
+        console.log("QQQ gcw refreshClusters");
+    } else if (event.data.clusterId) {
+        postMessage({
+            message: "expansion",
+            content: {
+                expansionZoom: superCluster.getClusterExpansionZoom(
+                    event.data.clusterId,
+                ),
+                center: event.data.center,
+            },
+        });
+    } else {
+        self.postMessage({
+            message: "moved",
+            content: superCluster.getClusters(event.data.bbox, event.data.zoom),
+        });
+        console.log("QQQ gcw updateClusters");
+    }
+};
